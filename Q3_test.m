285e5e5379a4e6b728f78414196f0f1386128f3f
Ucherl <hsnu126502@gmail.com>
2016-10-27 20:43:21 +0800

    Add files via upload

---
+++ Q3_test.m
@@ -0,0 +1,36 @@
+function output = Q3_test(A,B)
+
+%UNTITLED Summary of this function goes here
+
+%   Detailed explanation goes here
+
+m = size(A,2);
+
+n = size(B,2);
+
+output = fzero(@sumzero,0);
+
+ 
+
+function  output2 = sumzero(x)
+
+sum1=0;
+sum2=0;
+for i =1:m
+
+    sum1= sum1 + sqrt( (A(1,i)-x)^2 + (A(2,i)-0)^2 );
+
+end
+
+for j =1:n
+
+    sum2= sum2 + sqrt( (B(1,j)-x)^2+ (B(2,j)-0)^2);
+
+end
+
+output2 = sum1-sum2;
+
+end
+
+end
+
